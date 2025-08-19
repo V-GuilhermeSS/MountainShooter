637cfa230a70a3b063d11d44e17f64667a1edb2f
Valmir Guilherme Soares da Silva <guilherme.s.live@live.com>
2025-08-19 16:25:58 -0300

    Changing Constants Color names

--- Code/menu.py
+++ Code/menu.py
@@ -4,7 +4,7 @@ import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from Code.Const import WIN_WIDTH, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
+from Code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -22,8 +22,8 @@ class Menu:
         while True:
             # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, 'Mountain', COLOR_WHITE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, 'Shooter', COLOR_WHITE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, 'Mountain', COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, 'Shooter', COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
